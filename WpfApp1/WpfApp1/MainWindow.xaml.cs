0fc33a961bdfd6fc6a63939014f33329024acb40
Smoz <Steve.Moseley@g.austincc.edu>
2017-07-12 17:05:47 -0500

    Wpf C# - Date Ordinal Suffix Adder
    Take the output of DatePicker and will add the ordinal suffix to it. Full Wpf C# solution.
    
    Ex: July 12, 2017 = July 12th, 2017

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void TextBoxDate()
+        {
+            string month = datePicker.SelectedDate.Value.ToString("MMMM");
+            int day = datePicker.SelectedDate.Value.Day;
+            string year = datePicker.SelectedDate.Value.ToString("yyyy");
+
+            textBox.Text = month + " " + getDaySuffix(day) + ", " + year;
+        }
+
+        private string getDaySuffix(int day)
+        {
+            switch (day)
+            {
+                case 1:
+                case 21:
+                case 31:
+                    return day + "st";
+                case 2:
+                case 22:
+                    return day + "nd";
+                case 3:
+                case 23:
+                    return day + "rd";
+                default:
+                    return day + "th";
+            }
+        }       
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            TextBoxDate();
+        }
+    }
+}
